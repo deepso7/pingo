700749bd3046740a849bdcb3776759f0ccd7209d
deepso <deeps.o7o8.y@gmail.com>
2025-03-31 16:27:36 +0530

    rm env set

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,14 @@ mod server;
 mod stun;
 
 fn init_log() {
-    use std::env;
     use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
-    if env::var("RUST_LOG").is_err() {
-        env::set_var("RUST_LOG", "pingo=debug");
-    }
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("pingo=debug"));
 
     tracing_subscriber::registry()
         .with(fmt::layer())
-        .with(EnvFilter::from_default_env())
+        .with(env_filter)
         .init();
 }
 
